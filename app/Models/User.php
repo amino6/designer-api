b60cb448f098ddabe03657f5b4f8802845e6e45a
amine h <hakmi5508@gmail.com>
2023-09-30 11:51:24 +0100

    adding the designs model and migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function designs()
+    {
+        return $this->hasMany(Design::class);
+    }
 }
---
+++ database/migrations/2023_09_30_104445_create_designs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('designs', function (Blueprint $table) {
+            $table->id();
+            $table->string('image');
+            $table->string('title')->nullable();
+            $table->string('slug')->nullable();
+            $table->string('description')->nullable();
+            $table->boolean('is_live')->default(false);
+            $table->foreignId('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('designs');
+    }
+};
